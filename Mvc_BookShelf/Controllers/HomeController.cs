4ba1d2897a6b4e0a49cb95aeb35fd8b8874a5d25
khanisha123 <khanisha@code.edu.az>
2022-01-15 00:35:59 +0400

    Add project files.

---
+++ Mvc_BookShelf/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc_BookShelf.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc_BookShelf.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var books = new List<Books>
+            {
+                new Books(){Id=1,BookName="Chess",Writer="Zweig"},
+                new Books(){Id=2,BookName="The Stand",Writer="Stephen King"},
+                new Books(){Id=3,BookName="The Death of Ivan Ilyich",Writer="Leo Tolstoy"},
+                new Books(){Id=4,BookName="The Brothers Karamazov",Writer="Fyodor Dostoevsky"},
+                new Books(){Id=5,BookName="The Gambler",Writer="Fyodor Dostoevsky"}
+            };
+            return View(books);
+        }
+    }
+}
---
+++ Mvc_BookShelf/Models/Books.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc_BookShelf.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string BookName  { get; set; }
+        public string Writer { get; set; }
+    }
+}
